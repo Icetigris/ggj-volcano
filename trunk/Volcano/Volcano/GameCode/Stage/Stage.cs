8fae3a7b51df842ab88ef162a1eb98001d7c9629
cjntaylor <cjntaylor@1316c4cb-9af7-6f23-e601-f9041ccac3ad>
2011-01-29 18:36:28 +0000

    Added stage and fixed grid namespace.

---
+++ trunk/Volcano/Volcano/GameCode/Stage/Stage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Volcano
+{
+    class Stage : DrawableGameComponent
+    {
+        public Character main { get; set; }
+        public List<Character> enemies { get; private set; }
+        //TODO: Add map/state/thing?
+
+        public Stage(Game game) : base(game)
+        {
+            this.enemies = new List<Character>();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            //TODO: Draw the map and volcano
+        }
+    }
+}
--- trunk/Volcano/Volcano/GameCode/Utility/Grid.cs
+++ trunk/Volcano/Volcano/GameCode/Utility/Grid.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Volcano.GameCode.Utility
+namespace Volcano
 {
     class Grid
     {
