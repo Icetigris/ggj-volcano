f9ccb93d62a0aab90e74e2caee489bfc9b1df950
benharris5@gmail.com <benharris5@gmail.com@1316c4cb-9af7-6f23-e601-f9041ccac3ad>
2011-01-30 02:02:47 +0000

    Made AI.

--- Volcano/Volcano/GameCode/Structures/Strucure.cs
+++ Volcano/Volcano/GameCode/Structures/Strucure.cs
@@ -14,16 +14,34 @@ namespace Volcano
         public int hp { get; private set; }
 
         /// <summary>
-        /// Hit area of building (doubles as the position).
+        /// Hit area of building.
         /// </summary>
         public Polygon hitArea { get; private set; }
+
+        /// <summary>
+        /// The center of the building.
+        /// </summary>
+        public Vector2 center { get; private set; }
         
         /// <summary>
         /// Make a new structure.
         /// </summary>
         /// <param name="game">The game.</param>
-        /// <param name="HitArea">The hit area for the building (doubles as the position).</param>
-        public Strucure(MainGame game, Polygon HitArea) : base(game) { hitArea = HitArea; }
+        /// <param name="HitArea">The hit area for the building.</param>
+        public Strucure(MainGame game, Vector2 center, double width, double height)
+            : base(game)
+        {
+            this.center = center;
+            Vector2 a, b, c, d;
+            a = center + new Vector2((float)width / 2, (float)height / 2);
+            b = center + new Vector2((float)-width / 2, (float)height / 2);
+            c = center + new Vector2((float)-width / 2, (float)-height / 2);
+            d = center + new Vector2((float)width / 2, (float)-height / 2);
+            hitArea.Lines.Add(new Line(a, b));
+            hitArea.Lines.Add(new Line(b, c));
+            hitArea.Lines.Add(new Line(c, d));
+            hitArea.Lines.Add(new Line(d, a));
+        }
 
         public abstract override void Draw(GameTime gameTime);
 
