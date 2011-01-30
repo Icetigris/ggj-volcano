1d5f62130793f1f6c37fa985fceefb26397aa045
cjntaylor <cjntaylor@1316c4cb-9af7-6f23-e601-f9041ccac3ad>
2011-01-30 02:48:54 +0000

    Added radii math to calculate hut positions.

--- Volcano/Volcano/GameCode/Globals.cs
+++ Volcano/Volcano/GameCode/Globals.cs
@@ -15,5 +15,18 @@ namespace Volcano
         public static int numLights = 0;
         public static int maxLights = 4;
         public static Lights[] lights;
+
+        public static Vector2 PointOnRadius(float radius, float theta)
+        {
+            return new Vector2(radius * (float) Math.Cos(theta), radius * (float) Math.Sin(theta));
+        }
+
+        public static Vector2 RandomPointBetweenRadii(float inner, float outer)
+        {
+            Random rand = new Random();
+            float rad = (float) rand.Next((int) inner, (int) outer);
+            float the = (float) rand.NextDouble() * MathHelper.TwoPi;
+            return Globals.PointOnRadius(rad, the);
+        }
     }
 }
